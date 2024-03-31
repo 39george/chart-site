d1803675c03123557a6a23418402ec065925ac4d
ghashy <obsidian.musicwork@gmail.com>
2024-03-31 12:34:53 +0300

    Add permissions tests

--- tests/api/permissions.rs
+++ tests/api/permissions.rs
@@ -1,1 +1,43 @@
-// TODO: implement integration tests with permissions
+use chart_site::config::Settings;
+
+use super::helpers::TestApp;
+
+#[tokio::test]
+async fn access_to_protected_with_login_is_allowed() {
+    // Create app
+    let config = Settings::load_configuration().unwrap();
+    let app = TestApp::spawn_app(config).await;
+    let http_client = reqwest::Client::builder()
+        .cookie_store(true)
+        .build()
+        .unwrap();
+
+    // Create and login admin
+    let admin = app.new_admin().await.unwrap();
+    assert_eq!(app.login_admin(&admin, &http_client).await.as_u16(), 200);
+
+    let response = http_client
+        .get(format!("{}/api/protected/health_check", app.address))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn access_to_protected_without_login_is_restricted() {
+    // Create app
+    let config = Settings::load_configuration().unwrap();
+    let app = TestApp::spawn_app(config).await;
+    let http_client = reqwest::Client::builder()
+        .cookie_store(true)
+        .build()
+        .unwrap();
+
+    let response = http_client
+        .get(format!("{}/api/protected/health_check", app.address))
+        .send()
+        .await
+        .unwrap();
+    assert_eq!(response.status().as_u16(), 403);
+}
