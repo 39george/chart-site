74b589864fc9acd0339fa759ff7f08c347ac9513
olegus199 <okremnyov@yandex.ru>
2024-03-31 17:07:46 +0300

    create basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
+import { Route, Router } from "@solidjs/router";
 import "./App.scss";
+import Navbar from "./Components/Navbar";
+import MainPage from "./Pages/MainPage";
+import ContactsPage from "./Pages/ContactsPage";
 
 function App() {
   return (
     <>
-      <div>hello, world!</div>
+      <Router root={Navbar}>
+        <Route
+          path="/"
+          component={MainPage}
+        />
+        <Route
+          path="/contacts"
+          component={ContactsPage}
+        />
+      </Router>
     </>
   );
 }
