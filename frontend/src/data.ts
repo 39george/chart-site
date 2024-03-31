0b611029e12ba561833b7d5fe9fa3bf03c2d5d12
olegus199 <okremnyov@yandex.ru>
2024-03-31 17:08:29 +0300

    init data file

---
+++ frontend/src/data.ts
@@ -0,0 +1,13 @@
+import { GenderOptions } from "./types";
+
+export const genders: GenderOptions[] = ["Любой", "Мужской", "Женский"];
+
+export const genres = [
+  "Поп",
+  "Поп-рок",
+  "Танцевальная",
+  "Хип-хоп/RnB/Trap",
+  "Шансон",
+];
+
+export const moods = ["Веселое", "Грустное", "Свэг"];
