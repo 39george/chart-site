e77b2f09302bcf9d7e2746a6c1b5572a72fdf449
olegus199 <okremnyov@yandex.ru>
2024-04-03 22:43:51 +0300

    Pass change step logic to child

--- frontend/admin/src/Components/UploadFormContainer.tsx
+++ frontend/admin/src/Components/UploadFormContainer.tsx
@@ -1,8 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import UploadFormComponent from "./UploadFormComponent";
+import { CurrentStep } from "../types";
 
 const UploadFormContainer: FC = () => {
-  return <UploadFormComponent />;
+  const [current_step, set_current_step] = useState<CurrentStep>(
+    CurrentStep.Step_1
+  );
+
+  function handle_change_step(step: CurrentStep) {
+    set_current_step(step);
+  }
+
+  return (
+    <UploadFormComponent
+      state={{ current_step: current_step, change_step: handle_change_step }}
+    />
+  );
 };
 
 export default UploadFormContainer;
