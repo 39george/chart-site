f3d161f926d1ed2a98629b1bb91f5b7722dac440
olegus199 <okremnyov@yandex.ru>
2024-04-06 12:37:20 +0300

    Improve logic of UploadFormContainer

--- frontend/admin/src/Components/UploadFormContainer.tsx
+++ frontend/admin/src/Components/UploadFormContainer.tsx
@@ -1,19 +1,83 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import UploadFormComponent from "./UploadFormComponent";
-import { CurrentStep } from "../types";
+import { IStep, ISteps, StepName } from "../types";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import { FilesUploaded } from "../state/files_uploaded_slice";
 
 const UploadFormContainer: FC = () => {
-  const [current_step, set_current_step] = useState<CurrentStep>(
-    CurrentStep.Step_1
+  const [steps, set_steps] = useState<ISteps>({
+    step_1: {
+      active: true,
+      path: "step_1",
+    },
+    step_2: {
+      active: false,
+      path: "step_2",
+    },
+    step_3: {
+      active: false,
+      path: "step_3",
+    },
+    step_4: {
+      active: false,
+      path: "step_4",
+    },
+  });
+  const [current_step, set_current_step] = useState<StepName>("step_1");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const files_uploaded = useSelector<RootState, FilesUploaded>(
+    (state) => state.files_uploaded
   );
 
-  function handle_change_step(step: CurrentStep) {
-    set_current_step(step);
+  function handle_change_step(step: IStep) {
+    if (!step.active) {
+      return;
+    }
+    set_current_step(step.path);
+    navigate(step.path);
   }
+
+  useEffect(() => {
+    if (files_uploaded.audio && files_uploaded.img) {
+      set_steps((prev) => ({
+        ...prev,
+        step_2: {
+          ...prev.step_2,
+          active: true,
+        },
+      }));
+    }
+  }, [files_uploaded]);
+
+  useEffect(() => {
+    if (location.pathname !== "/upload_new_song/step_1") {
+      navigate("step_1");
+    }
+  }, []);
+
+  useEffect(() => {
+    const handle_before_unload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      e.returnValue = "";
+    };
+
+    window.addEventListener("beforeunload", handle_before_unload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handle_before_unload);
+    };
+  }, []);
 
   return (
     <UploadFormComponent
-      state={{ current_step: current_step, change_step: handle_change_step }}
+      state={{
+        steps: steps,
+        current_step: current_step,
+        change_step: handle_change_step,
+      }}
     />
   );
 };
