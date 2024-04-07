4b9855e72e3ca74ec6c90a4cf1fa2e27b402a5b6
olegus199 <okremnyov@yandex.ru>
2024-04-07 11:31:46 +0300

    Import UploadStep3, add API requests

--- frontend/admin/src/App.tsx
+++ frontend/admin/src/App.tsx
@@ -5,8 +5,62 @@ import MainPage from "./Pages/MainPage";
 import UploadNewPage from "./Pages/UploadNewPage";
 import UploadStep1 from "./Components/UploadStep1";
 import UploadStep2 from "./Components/UploadStep2";
+import UploadStep3 from "./Components/UploadStep3";
+import { useSelector } from "react-redux";
+import { RootState } from "./state/store";
+import { ISongData } from "./state/song_data_slice";
+import useAxios from "./Hooks/APIRequests";
+import { RequestMethods } from "./types";
+import { API_URL } from "./config";
+import { useEffect, useState } from "react";
 
 function App() {
+  const song_data = useSelector<RootState, ISongData>(
+    (state) => state.song_data
+  );
+  const [genres_list, set_genres_list] = useState<string[]>([]);
+  const [moods_list, set_moods_list] = useState<string[]>([]);
+  const { error_data: genres_error, fetch_data: fetch_genres } = useAxios();
+  const { error_data: moods_error, fetch_data: fetch_moods } = useAxios();
+
+  async function get_genres_moods() {
+    try {
+      const [genres, moods] = await Promise.all([
+        get_genres_list(),
+        get_moods_list(),
+      ]);
+      set_genres_list(genres);
+      set_moods_list(moods);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  async function get_genres_list() {
+    const response = await fetch_genres(
+      RequestMethods.Get,
+      `${API_URL}/open/genres`
+    );
+    if (response?.status === 200) {
+      return response.data;
+    }
+  }
+
+  async function get_moods_list() {
+    const response = await fetch_moods(
+      RequestMethods.Get,
+      `${API_URL}/open/moods`
+    );
+    if (response?.status === 200) {
+      return response.data;
+    }
+  }
+
+  // Call API
+  useEffect(() => {
+    get_genres_moods();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -28,11 +82,17 @@ function App() {
             />
             <Route
               path="step_2"
-              element={<UploadStep2 />}
+              element={<UploadStep2 song_data={song_data} />}
             />
             <Route
               path="step_3"
-              element={<div>step 3</div>}
+              element={
+                <UploadStep3
+                  song_data={song_data}
+                  genres_list={genres_list}
+                  moods_list={moods_list}
+                />
+              }
             />
             <Route
               path="step_4"
