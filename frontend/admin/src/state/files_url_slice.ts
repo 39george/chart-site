1d05830a5e1c9eccb641fcc334df66c932acf412
olegus199 <okremnyov@yandex.ru>
2024-04-07 23:09:23 +0300

    Add files url slice

---
+++ frontend/admin/src/state/files_url_slice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface InitialState {
+  audio: string;
+  img: string;
+}
+
+const initial_state: InitialState = {
+  audio: "",
+  img: "",
+};
+
+const files_url_slice = createSlice({
+  name: "files_url",
+  initialState: initial_state,
+  reducers: {
+    set_audio_url: (state: InitialState, action: PayloadAction<string>) => {
+      state.audio = action.payload;
+    },
+    set_img_url: (state: InitialState, action: PayloadAction<string>) => {
+      state.img = action.payload;
+    },
+  },
+});
+
+export const { set_audio_url, set_img_url } = files_url_slice.actions;
+
+export default files_url_slice.reducer;
