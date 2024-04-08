77cc5166f90479b846de5309bc1997766cb7270c
olegus199 <okremnyov@yandex.ru>
2024-04-08 12:15:06 +0300

    Import new reducers to global store

--- frontend/admin/src/state/store.ts
+++ frontend/admin/src/state/store.ts
@@ -1,13 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import song_data_reducer from "./song_data_slice";
 import genres_moods_changed_reducer from "./genres_moods_changed_slice";
-import files_url_slice from "./files_url_slice";
+import files_url_reducer from "./files_url_slice";
+import song_submit_data_reducer from "./song_submit_data_slice";
+import song_list_updated_reducer from "./song_list_updated_slice";
 
 const store = configureStore({
   reducer: {
     song_data: song_data_reducer,
     genres_moods_changed: genres_moods_changed_reducer,
-    files_url: files_url_slice,
+    files_url: files_url_reducer,
+    song_submit_data: song_submit_data_reducer,
+    song_list_updated: song_list_updated_reducer,
   },
 });
 
