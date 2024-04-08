85c48d4a8b1484823165bdf8d576d8e7dd2c3a99
olegus199 <okremnyov@yandex.ru>
2024-04-08 21:54:59 +0300

    Import auido player styles

--- frontend/client_react/src/main.tsx
+++ frontend/client_react/src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./state/store.ts";
+import "react-h5-audio-player/src/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
