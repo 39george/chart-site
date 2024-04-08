121beeb3d820332b28d89fad55d84af314a19cfb
olegus199 <okremnyov@yandex.ru>
2024-04-08 21:57:52 +0300

    Import and customize react player

--- frontend/client_react/src/Pages/MainPage.tsx
+++ frontend/client_react/src/Pages/MainPage.tsx
@@ -1,9 +1,50 @@
 import styles from "./MainPage.module.scss";
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import Filters from "../Components/Filters";
 import SongsList from "../Components/SongsList";
+import AudioPlayer from "react-h5-audio-player";
+import {
+  IoPlay,
+  IoPause,
+  IoPlayBack,
+  IoPlayForward,
+  IoPlaySkipBack,
+  IoPlaySkipForward,
+  IoVolumeHigh,
+  IoVolumeMute,
+} from "react-icons/io5";
+import { RiLoopLeftFill } from "react-icons/ri";
+import { useSelector } from "react-redux";
+import { RootState } from "../state/store";
+import H5AudioPlayer from "react-h5-audio-player";
+import { ISong } from "../types";
 
 const MainPage: FC = () => {
+  const songs = useSelector<RootState, ISong[]>((state) => state.songs.songs);
+  const audio_url = useSelector<RootState, string>(
+    (state) => state.song_url.url
+  );
+  const current_song_id = useSelector<RootState, number>(
+    (state) => state.current_song_data.id
+  );
+  const song_playing = useSelector<RootState, boolean>(
+    (state) => state.current_song_data.is_palying
+  );
+  const audio_player_ref = useRef<H5AudioPlayer>(null);
+
+  // FIXME strange logic, why !song_playing???
+  useEffect(() => {
+    if (audio_url) {
+      if (audio_player_ref.current?.audio.current) {
+        if (!song_playing) {
+          audio_player_ref.current.audio.current.pause();
+        } else {
+          audio_player_ref.current.audio.current.play();
+        }
+      }
+    }
+  }, [song_playing]);
+
   return (
     <section className={styles.main_page}>
       <div className={styles.content}>
@@ -11,6 +52,93 @@ const MainPage: FC = () => {
         <Filters />
         <SongsList />
       </div>
+      {current_song_id !== -1 && (
+        <div className={styles.player_meta_info}>
+          <div className={styles.image_wrapper}>
+            <img
+              src={songs.find((song) => song.id === current_song_id)?.cover_url}
+              alt=""
+            />
+          </div>
+          <p className={styles.name}>
+            {songs.find((song) => song.id === current_song_id)?.name}
+          </p>
+        </div>
+      )}
+      <AudioPlayer
+        ref={audio_player_ref}
+        src={audio_url}
+        style={{
+          display: `${current_song_id !== -1 ? "flex" : "none"}`,
+        }}
+        showFilledVolume
+        layout="horizontal"
+        customIcons={{
+          play: (
+            <IoPlay
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          pause: (
+            <IoPause
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          rewind: (
+            <IoPlayBack
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          forward: (
+            <IoPlayForward
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          previous: (
+            <IoPlaySkipBack
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          next: (
+            <IoPlaySkipForward
+              className={styles.playback_icon}
+              style={{
+                width: "1.5rem",
+                height: "1.5rem",
+              }}
+            />
+          ),
+          loop: <RiLoopLeftFill className={styles.playback_icon} />,
+          loopOff: (
+            <RiLoopLeftFill
+              className={styles.playback_icon}
+              style={{ color: "#959595" }}
+            />
+          ),
+          volume: <IoVolumeHigh className={styles.playback_icon} />,
+          volumeMute: <IoVolumeMute className={styles.playback_icon} />,
+        }}
+      />
     </section>
   );
 };
