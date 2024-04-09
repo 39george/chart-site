d05345a1b4b7aedfd237d47070568578d06647c8
olegus199 <okremnyov@yandex.ru>
2024-04-09 09:08:36 +0300

    Add new loading state

--- frontend/admin/src/state/permissions_slice.ts
+++ frontend/admin/src/state/permissions_slice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface InitialState {
   premissions: boolean;
+  is_loading: boolean;
 }
 
 const initial_state: InitialState = {
   premissions: false,
+  is_loading: true,
 };
 
 const permissions_slice = createSlice({
@@ -15,9 +17,12 @@ const permissions_slice = createSlice({
     set_permissions: (state: InitialState, action: PayloadAction<boolean>) => {
       state.premissions = action.payload;
     },
+    set_is_loading: (state: InitialState, action: PayloadAction<boolean>) => {
+      state.is_loading = action.payload;
+    },
   },
 });
 
-export const { set_permissions } = permissions_slice.actions;
+export const { set_permissions, set_is_loading } = permissions_slice.actions;
 
 export default permissions_slice.reducer;
