7eefc83b3897e56d7f406ace3e7d13b0682e3064
olegus199 <okremnyov@yandex.ru>
2024-04-11 11:49:42 +0300

    Add forbidden chars

--- frontend/admin/src/config.ts
+++ frontend/admin/src/config.ts
@@ -2,3 +2,5 @@ export const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 export const MAX_RETRIES = 7;
 export const RETRY_DELAY_MS = 1000;
+
+export const FORBIDDEN_CHARS = /[\/()"<>\\{};:]/;
