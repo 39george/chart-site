30f2a691628c560bbe34a919e4a9d7df672bb6a5
olegus199 <okremnyov@yandex.ru>
2024-04-12 14:14:57 +0300

    Finalized color themes impl

--- frontend/client_react/src/Components/MainLayout.tsx
+++ frontend/client_react/src/Components/MainLayout.tsx
@@ -1,12 +1,26 @@
+import styles from "./MainLayout.module.scss";
 import { FC } from "react";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import { ColorThemes } from "../types";
+import { RootState } from "../state/store";
+import { useSelector } from "react-redux";
 
 const MainLayout: FC = () => {
+  const color_theme = useSelector<RootState, ColorThemes>(
+    (state) => state.color_theme.theme
+  );
+
   return (
     <>
       <Navbar />
       <Outlet />
+      <div className={styles.decor_container}>
+        <div
+          className={styles.bg_decor}
+          style={{ filter: `invert(${color_theme === "light" ? 0 : 1})` }}
+        ></div>
+      </div>
     </>
   );
 };
