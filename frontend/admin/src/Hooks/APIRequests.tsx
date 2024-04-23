456cf5464d49ce7d6e53cfe0eaffcc50c75f66fb
olegus199 <okremnyov@yandex.ru>
2024-04-23 14:57:56 +0300

    Update error handling logic in useAxios

--- frontend/admin/src/Hooks/APIRequests.tsx
+++ frontend/admin/src/Hooks/APIRequests.tsx
@@ -4,13 +4,14 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { wait } from "../helpers";
 
 const useAxios = () => {
+  const [response_status, set_response_status] = useState(0);
   const [error_data, set_error_data] = useState<string>("");
 
   const fetch_data = async (
     config: AxiosRequestConfig,
     attempts: number = 1
   ) => {
-    let response: AxiosResponse;
+    let response: AxiosResponse | undefined = undefined;
     try {
       switch (config.method) {
         case "GET":
@@ -29,15 +30,18 @@ const useAxios = () => {
           switch (error.response.status) {
             case 400:
               console.error("Bad request:", error.response);
+              set_response_status(400);
               break;
             case 401:
               console.error("Unathorized: ", error.response);
+              set_response_status(401);
               set_error_data(
                 "Запрос запрещен. Пожалуйста, авторизуйтесь и попробуйте еще раз"
               );
               break;
             case 403:
               console.error("Forbidden:", error.response);
+              set_response_status(403);
               set_error_data(
                 "Запрос запрещен. У вас нет прав для выполнения данного запроса"
               );
@@ -48,15 +52,18 @@ const useAxios = () => {
                 error.config?.url,
                 error.response
               );
+              set_response_status(404);
               break;
             case 415:
               console.error("Unsupported media type: ", error.response);
+              set_response_status(415);
               break;
             case 422:
               console.error(
-                "Trying to delete data used somewhere: ",
+                "Trying to delete data used somewhere",
                 error.response
               );
+              set_response_status(422);
               set_error_data(
                 "Вы пытаетесь удалить данные, которые используются в загруженных песнях. Пожалуйста, сначала поменяйте данные в песне или удалите её."
               );
@@ -67,6 +74,7 @@ const useAxios = () => {
                 fetch_data(config, attempts + 1);
               } else {
                 console.error("Server error:", error.response);
+                set_response_status(500);
                 set_error_data(
                   "На нашем сервере произошла ошибка. Мы делаем всё возможное, чтобы её исправить. Пожалуйста, повторите ваш запрос позднее"
                 );
@@ -87,7 +95,7 @@ const useAxios = () => {
             await wait(RETRY_DELAY_MS);
             fetch_data(config, attempts + 1);
           } else {
-            console.error("Server is not responding: ", error.message);
+            console.error("Server is not responding:", error.message);
             set_error_data(
               "Сервер не отвечает, пожалуйста, обновите страницу и попробуйте еще раз"
             );
@@ -101,7 +109,13 @@ const useAxios = () => {
     }
   };
 
-  return { error_data, fetch_data };
+  return {
+    error_data,
+    set_error_data,
+    response_status,
+    set_response_status,
+    fetch_data,
+  };
 };
 
 export default useAxios;
